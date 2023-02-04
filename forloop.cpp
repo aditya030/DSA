c2ffb3e92e74d5d20a25fd57fbaf894859523e21
Aditya Sinha <61179961+aditya030@users.noreply.github.com>
2023-02-04 16:34:35 +0530

    Add files via upload

---
+++ forloop.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // for(int i=0;i<5;i++){
+    //     cout<<i<<endl;
+    // }
+
+
+    // for(int i=1000000000;i>0;i=i/2){
+        // cout<<i<<endl;
+
+
+        int n;
+        if(cin>>n){
+            cout<<"adiiii"<<endl;
+        }
+    } 
