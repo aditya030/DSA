c7c70cd215d345a2f5dbfb4ec943a773e28bcd31
Aditya Sinha <61179961+aditya030@users.noreply.github.com>
2023-02-05 23:45:37 +0530

    Add files via upload

---
+++ ifelse.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int score;
+
+    // cout << "enter the live score" << endl;
+    // cin >> score;
+
+    // if(score>300){
+    //     cout<<"india wins"<<endl;
+    // }
+    // else if(score<300){
+    //      cout<<"pakistan lost";
+
+// int marks;
+// cout<<"enter youyr marks"<<endl;
+// cin>>marks;
+// if(marks>=90){
+//     cout<<"A GRADE";
+// }
+// else if(marks>=80){
+//     cout<<"B GRADE";
+// }
+// else if(marks>=60){
+//     cout<<"C GRADE";
+// }
+// else{
+//     cout<<"FAIL";
+// }
+
+int bro_num;
+cout<<"Enter the no_of brothers"<<endl;
+cin>>bro_num;
+if(bro_num==1){
+    cout<<"baat bann jayegi"<<endl;;
+}
+else if(bro_num>=1){
+    cout<<"baat nhi bangei"<<endl;;
+}else{
+    cout<<"koy bhai bhi bna luo"<<endl;
+}
+
+    }
+    
+
