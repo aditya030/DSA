e503cfbd2392c5a2d231ba379a32b2e14302c753
Aditya Sinha <61179961+aditya030@users.noreply.github.com>
2023-05-21 22:21:17 +0530

    Add files via upload

---
+++ ratinamaze.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vectors>
+using namespace std;
+
+void mazesolver(int arr[][3],int rows,int columns){
+
+  if(i,j==0){
+      return;
+  }
+
+  string str =" ";
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+int main(){
+ int arr[3]3]={{1,1,0},{0,1,1},{1,1,1}};
+ int rows = 3;
+ int columns = 3;
+ vector<vector<bool> > visited
+mazesolver(arr,rows,columns);
+cout<<"the direction of the moveemnt is: "<<endl;
+
+
+
+
+
+
+}
