c7c480d0ffca8a86edaf7ef44c891116676a2f5e
Aditya Sinha <61179961+aditya030@users.noreply.github.com>
2023-08-17 15:14:12 +0530

    Add files via upload

---
+++ interleave2halfqueue.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+void interleaveQueue(queue<int> &q){
+    //STEP A separate both halves
+    int n = q.size();
+    if(q.empty()){
+        return;
+    }
+    int k = n / 2;
+    int count = 0;
+    queue<int> q2;
+
+    while(!q.empty()){ // Corrected the loop condition here
+        int temp = q.front();
+        q.pop();
+        q2.push(temp);
+        count++;
+
+        if(count == k)
+            break;
+    }
+
+    //step 2: start intereaving
+    while(!q.empty() && !q2.empty()){
+        int first = q2.front();
+        q2.pop();
+        q.push(first);
+
+        int second = q.front();
+        q.pop();
+        q2.push(second);
+    }
+
+    if(n & 1){
+        int element = q.front();
+        q.pop();
+        q.push(element);
+    }
+} 
+
+int main(){
+    queue<int> q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4);
+    q.push(5);
+    q.push(6);
+    q.push(7);
+    q.push(8);
+
+    interleaveQueue(q);
+
+    cout << "Printing Queue" << endl;
+    while(!q.empty()){
+        cout << q.front() << " ";
+        q.pop();
+    }
+    cout << endl;
+
+    return 0;
+}
