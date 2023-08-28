bb56b169b5e9751228b8902dfa394c759803aaf0
Aditya Sinha <61179961+aditya030@users.noreply.github.com>
2023-08-28 22:31:43 +0530

    Add files via upload

---
+++ diameterofbtree.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+int maxDepth(TreeNode* root) {
+        if(root == NULL){
+            return 0;
+        }
+
+        int leftheight = maxDepth(root->left);
+        int rightheight = maxDepth(root->right);
+        int ans = max(leftheight,rightheight)+1;
+        return ans;
+        
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+
+        if(root == NULL){
+            return 0;
+        }
+        int op1 = diameterOfBinaryTree(root->left);
+        int op2 = diameterOfBinaryTree(root->right);
+        int op3 = maxDepth(root->left)+maxDepth(root->right);
+
+        int ans = max(op1,max(op2,op3));
+        return ans;
+        
+
+    }
+};
