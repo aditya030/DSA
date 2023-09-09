ff8fac322f43a08da6e198fff949acf15b50cc42
Aditya Sinha <61179961+aditya030@users.noreply.github.com>
2023-09-10 01:36:23 +0530

    Add files via upload

---
+++ topviewtree.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+void printtopview(Node* root){
+    if(root == NULL){
+        return;
+    }
+    // create a map for storing horizo node se 
+    map<int , int> topnode;
+
+    //level order
+    // we will store a pair consisting of node and horizontal distance
+
+    queue< pair<Node* , int>> q;
+    q.push(make_pair(root,0));
+
+    while(!q.empty()){
+        pair<Node* , int> temp = q.front();
+        q.pop();
+
+
+        Node* frontNode = temp.first();
+        int hd = temp.second();
+
+
+        //jo bhi hd dist. check if ans is there or not
+
+        if(topnode.find(hd)==topnode.end()){
+            // create entry
+
+            topnode[hd]=frontNode->data;
+            if(frontNode ->left)
+            q.push(make_pair(frontNode->left,hd-1));
+
+            if(frontNode->right)
+            q.push(make_pair(frontNode->right,hd+1));
+
+        }
+
+
+        for(auto i:topnode){
+            cout<<i.first<<" "<<i.second;
+        }
+
+    }
+
+
+}
+int main(){
+
+}
